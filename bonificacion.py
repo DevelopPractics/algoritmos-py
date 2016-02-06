d16fe0f238d7fd75d9364db1fefcd6ca95a89899
Carlos Navia <cenaviat@gmail.com>
2016-02-05 19:44:23 -0500

    segunda condicional

--- bonificacion.py
+++ bonificacion.py
@@ -5,3 +5,6 @@ MV = int(raw_input("Monto venta: "))
 if MV >= 0 and MV <1000:
 	TB = (0*MV)/100
 	print "Tu bonificacion es: ", TB
+elif MV >= 1000 and MV < 5000:
+	TB = (3*MV)/100
+	print "Tu bonificacion es: ", TB
